1eedd68a7b73b359710daf16a3eea2f066ca9a13
Martin KOller <kollermartin00@gmail.com>
2019-08-17 20:54:23 +0200

    animation

--- Effects.js
+++ Effects.js
@@ -1,3 +1,13 @@
+
 $(".toggleHeroes").click(function(){
-  $("#allHeroes").slideToggle("slow")
+  $("#allHeroes").slideToggle("1000",function(){
+    // $("html,body").animate({
+    //   scrollTop:$("#allHeroes").offset().top
+    // },2000)
+  })
+});
+$(".toggleHeroes").click(function(e){
+  $("html,body").animate({
+      scrollTop: $("#allHeroes").offset().top
+    },1000)
 });

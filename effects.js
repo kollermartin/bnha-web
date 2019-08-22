c6af3e45f22213e33934b282f9f91dbd3e8f182b
Martin KOller <kollermartin00@gmail.com>
2019-08-22 17:04:37 +0200

    Hi my friend

--- effects.js
+++ effects.js
@@ -1,32 +1,27 @@
 // noinspection JSDeprecatedSymbols
 
-$(".toggleHeroes").click(function(){
-  $("#allHeroes").slideToggle("1000",function(){
 
-  })
-});
- // noinspection JSDeprecatedSymbols
-$(".toggleHeroes").click(function () {
+$(".toggleHeroes").click(function(){
+  $("#allHeroes").slideToggle("1000");
     $("html,body").animate({
         scrollTop: $("#allHeroes").offset().top
     }, 1000)
 });
 
-
 $(".toggleVillains").click(function(){
-    $("#allVillains").slideToggle("1000",function(){
-
-    })
-});
-
-// noinspection JSDeprecatedSymbols
-$(".toggleVillains").click(function () {
+    $("#allVillains").slideToggle("1000");
     $("html,body").animate({
         scrollTop: $("#allVillains").offset().top
-    }, 1000)
-});
-$(".cardButton").click(function () {
 
+    }, 1000);
+});
+
+$(".toggleHeroes,.toggleVillains").click(function () {
+    $(".card-2").slideUp();
+    $(".card").slideDown();
+});
+
+$(".cardButton").click(function () {
     $(this).parents(".card").slideUp("slow",function () {
         $(this).siblings(".card-2").slideDown("slow");
     });
@@ -34,8 +29,9 @@ $(".cardButton").click(function () {
 });
 
 $(".card2Button").click(function () {
-    $(this).parents(".card-2").siblings(".card").slideDown("slow");
     $(this).parents(".card-2").slideUp("slow");
-})
+    $(this).parents(".card-2").siblings(".card").slideDown("slow");
+
+});
 
 
